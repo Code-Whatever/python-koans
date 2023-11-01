b34f51b1b5ea18aa271a86e45371ff4e259aef54
Andrew <andrewnovosel90@gmail.com>
2023-11-01 11:47:38 -0400

    Testing 1 koan in 1 file to run 1 command

--- index.py
+++ index.py
@@ -1,1 +1,9 @@
 # Using this file for creating and testing koans.
+
+def test_assign_value_to_variable():
+    # Assign a value to the variable.
+    movie = "The Firm"
+    # To display the value of the variable, use the "print" function.
+    print(movie)
+# To see the value of the variable in the terminal, we need to call the function.
+test_assign_value_to_variable()
---
+++ run.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+python3 -B analyze_koans.py
