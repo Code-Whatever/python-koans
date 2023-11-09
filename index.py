f9a3a3e64559e833b5df0f397a01f8fd4f571221
Andrew <andrewnovosel90@gmail.com>
2023-11-09 15:42:21 -0500

    Getting rid of all comments after line 1

--- index.py
+++ index.py
@@ -4,9 +4,6 @@ def determine_if_valid_answer():
     valid_answer = ("abcdefghijklmnopqrstuvwxyz ABCDEFGHIJKLMNOPQRSTUVWXYZ 1234567890 ,.' ()")
 
 def test_assign_value_to_variable():
-    # Assign a value to the variable.
-    movie = "The Breakfast Club" # Write the title of a movie that you enjoy - Ex: movie = "Goodfellas"
-    # To display the value of the variable, use the "print" function.
+    movie = "The Breakfast Club" 
     print(movie)
-# To see the value of the variable in the terminal, we need to call the function.
 test_assign_value_to_variable()
