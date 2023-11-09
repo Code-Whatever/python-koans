035ea7e46180aa7aeea70d4620e41bc4305bbe0a
Andrew <andrewnovosel90@gmail.com>
2023-11-09 15:41:15 -0500

    Writing function for valid koan answers

--- index.py
+++ index.py
@@ -1,8 +1,11 @@
 # Using this file for creating and testing koans.
 
+def determine_if_valid_answer():
+    valid_answer = ("abcdefghijklmnopqrstuvwxyz ABCDEFGHIJKLMNOPQRSTUVWXYZ 1234567890 ,.' ()")
+
 def test_assign_value_to_variable():
     # Assign a value to the variable.
-    movie = "____" # Write the title of a movie that you enjoy - Ex: movie = "Goodfellas"
+    movie = "The Breakfast Club" # Write the title of a movie that you enjoy - Ex: movie = "Goodfellas"
     # To display the value of the variable, use the "print" function.
     print(movie)
 # To see the value of the variable in the terminal, we need to call the function.
