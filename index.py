a063d60fbdceedb3c0c637cff29cc771e9abf942
Andrew <andrewnovosel90@gmail.com>
2023-11-15 14:09:42 -0500

    Currently creating more koans

--- index.py
+++ index.py
@@ -1,7 +1,16 @@
 # Using this file for creating and testing koans.
 import unittest
 
-class AboutVariables(unittest.TestCase):
+class AboutDataTypes(unittest.TestCase):
+    def test_string_using_double_quotes(self):
+        string = "spam"
+        self.assertEqual(True, isinstance(string, str))
+
+    def test_string_using_single_quotes(self):
+        string = 'eggs'
+        self.assertEqual(True, isinstance(string, str))
+    
+    #def test_assign_multiline_string_using_single_quotes(self):
 
     def test_assign_value_to_variable(self):
         self.assertTrue(True)
